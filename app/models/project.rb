2cb99c317ab74aebc7841ebf1297301354addaeb
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-07-30 15:19:13 -0400

    added costume validations for projects and schedule for start and end date

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,8 +4,11 @@ class Project < ApplicationRecord
 
     validates :name, presence: true, uniqueness: true
     validates :address, presence: true
-    validates :project_number, presence: true
+    validates :project_number, presence: true, numericality: true
     validates :construction_type, presence: true, inclusion: {in: ['New Construction', 'Tenent']}
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    validate :start_date_is_not_in_past, :end_date_is_not_before_start_date
 
     def start_time_format
         self.start_date.strftime("%B %d, %Y")
@@ -13,5 +16,18 @@ class Project < ApplicationRecord
 
     def end_time_format
         self.end_date.strftime("%B %d, %Y")
+    end
+
+    #validators
+    def start_date_is_not_in_past
+        if start_date.present? && start_date < Date.today
+            errors.add(:start_date, "can't be in the past")
+        end
+    end
+
+    def end_date_is_not_before_start_date
+        if start_date < end_date
+            errors.add(:end_date, "can't be set before start date")
+        end
     end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,5 +6,26 @@ class Schedule < ApplicationRecord
   validates :project_id, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :start_date_is_not_in_past, :end_date_is_not_before_start_date
 
+  def start_time_format
+    self.start_date.strftime("%B %d, %Y")
+  end
+
+  def end_time_format
+      self.end_date.strftime("%B %d, %Y")
+  end
+
+  #validators
+  def start_date_is_not_in_past
+      if start_date.present? && start_date < Date.today
+          errors.add(:start_date, "can't be in the past")
+      end
+  end
+
+  def end_date_is_not_before_start_date
+      if start_date < end_date
+          errors.add(:end_date, "can't be set before start date")
+      end
+  end
 end
